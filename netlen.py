4f14c974e8c71ac5cfbdc17129a9e581f1416d3b
Richmond <andrew.richmond@nokia.com>
2018-02-06 13:28:04 -0700

    netlen ipaddr function passing all unittests

--- netlen.py
+++ netlen.py
@@ -7,10 +7,35 @@ def netmask(mask):
     ipv4_min, ipv4_max = 0, 32
 
     if not isinstance(mask, int):
-        raise TypeError("Mask must be of type 'int'. Received: %s", mask)
+        raise TypeError("mask must be of type 'int'. Received: {}".format(mask))
     if mask < ipv4_min or mask > ipv4_max:
-        raise ValueError("IPv4 masks must be within range %d <= mask <= %d",
-                ipv4_min, ipv4_max)
+        raise ValueError(
+            "IPv4 masks must be within range {} <= mask <= {}".format(
+                ipv4_min, ipv4_max))
 
     return (2 ** mask - 1) << (ipv4_max - mask)
 
+def ipaddr(address):
+    """Convert IP address to its numerical value"""
+    ipv4_min, ipv4_max = 0, 255
+    ipv4_addr_len = 4
+    byte_len = 8
+
+    if not isinstance(address, str):
+        raise TypeError("address must be of type 'str'. Received {}".format(
+            address))
+    # raises ValueError if int not found
+    addr_bytes = [int(byte) for byte in address.split(".")]
+    if len(addr_bytes) != ipv4_addr_len:
+        raise ValueError("Invalid address length")
+    for byte in addr_bytes:
+        if byte < ipv4_min or byte > ipv4_max:
+            raise ValueError("Invalid address element '{}'".format(byte))
+
+    base = 0
+    for byte in addr_bytes[:-1]:
+        base += byte
+        base <<= byte_len
+    base += addr_bytes[-1]
+
+    return base
--- test_netlen.py
+++ test_netlen.py
@@ -1,11 +1,12 @@
 """
 Tests for netlen.py
 """
-import netlen
 import unittest
 
+import netlen
+
 class TestNetMask(unittest.TestCase):
-    def test_range(self):
+    def test_input(self):
         with self.assertRaises(TypeError):
             netlen.netmask("dog")
         with self.assertRaises(TypeError):
@@ -19,7 +20,7 @@ class TestNetMask(unittest.TestCase):
         four_byte_min = (2**0) - 1
         four_byte_max = (2**32) - 1
         self.assertEqual(netlen.netmask(0), four_byte_min)
-        self.assertEqual(netlen.netmask(32), four_byte_max) 
+        self.assertEqual(netlen.netmask(32), four_byte_max)
         self.assertEqual(netlen.netmask(8),  0b11111111000000000000000000000000)
         self.assertEqual(netlen.netmask(23), 0b11111111111111111111111000000000)
 
@@ -30,16 +31,23 @@ class TestIPAddress(unittest.TestCase):
         with self.assertRaises(TypeError):
             netlen.ipaddr([10, 10, 10, 10])
         with self.assertRaises(ValueError):
+            netlen.ipaddr("10.10.10.1O")
+        with self.assertRaises(ValueError):
             netlen.ipaddr("10.10.10.666")
+        with self.assertRaises(ValueError):
+            netlen.ipaddr("-10.10.10.66")
         with self.assertRaises(ValueError):
             netlen.ipaddr("10.10.10")
         with self.assertRaises(ValueError):
             netlen.ipaddr("10.10.10.10.10")
-        with self.assertRaises(ValueError):
-            netlen.ipaddr("10.10.10.1O")
 
-
-
+    def test_values(self):
+        four_byte_min = (2**0) - 1
+        four_byte_max = (2**32) - 1
+        self.assertEqual(netlen.ipaddr("0.0.0.0"), four_byte_min)
+        self.assertEqual(netlen.ipaddr("255.255.255.255"), four_byte_max)
+        self.assertEqual(netlen.ipaddr("10.10.10.10"),
+                         0b00001010000010100000101000001010)
 
 if __name__ == "__main__":
     unittest.main()
