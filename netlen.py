26cb30d6d2e29b7fec070014931b16717d377a1c
Richmond <andrew.richmond@nokia.com>
2018-02-06 15:00:23 -0700

    refactored netlen to use global variables, cutting down on repetative local vars

--- netlen.py
+++ netlen.py
@@ -2,59 +2,57 @@
 Basic utility for seeing address for a given IPv4 address and netmask
 """
 
+BYTE_SIZE = 8
+BYTE_MIN_SIZE = 0
+BYTE_MAX_SIZE = 2**BYTE_SIZE - 1
+
+IPV4_ADDR_LEN = 4
+IPV4_MIN_LEN = 0
+IPV4_MAX_LEN = BYTE_SIZE * IPV4_ADDR_LEN
+IPV4_MIN_VAL = 0
+IPV4_MAX_VAL = 2**IPV4_MAX_LEN - 1
+
 def netmask(mask):
     """Convert mask prefix to masks numerical value"""
-    ipv4_min, ipv4_max = 0, 32
-
     if not isinstance(mask, int):
         raise TypeError("mask must be of type 'int'. Received: {}".format(mask))
-    if mask < ipv4_min or mask > ipv4_max:
-        raise ValueError(
-            "IPv4 masks must be within range {} <= mask <= {}".format(
-                ipv4_min, ipv4_max))
+    if mask < IPV4_MIN_LEN or mask > IPV4_MAX_LEN:
+        raise ValueError("mask not within range")
 
-    return (2 ** mask - 1) << (ipv4_max - mask)
+    return (2 ** mask - 1) << (IPV4_MAX_LEN - mask)
 
 def ipaddr(address):
     """Convert IP address to its numerical value"""
-    ipv4_min, ipv4_max = 0, 255
-    ipv4_addr_len = 4
-    byte_len = 8
-
     if not isinstance(address, str):
         raise TypeError("address must be of type 'str'. Received {}".format(
             address))
     # raises ValueError if int not found
     addr_bytes = [int(byte) for byte in address.split(".")]
-    if len(addr_bytes) != ipv4_addr_len:
+    if len(addr_bytes) != IPV4_ADDR_LEN:
         raise ValueError("Invalid address length")
     for byte in addr_bytes:
-        if byte < ipv4_min or byte > ipv4_max:
+        if byte < BYTE_MIN_SIZE or byte > BYTE_MAX_SIZE:
             raise ValueError("Invalid address element '{}'".format(byte))
 
     base = 0
     for byte in addr_bytes[:-1]:
         base += byte
-        base <<= byte_len
+        base <<= BYTE_SIZE
     base += addr_bytes[-1]
 
     return base
 
 def to_address(addr_int):
     """Convert from address' numerical value to human readable form"""
-    ipv4_min, ipv4_max = 0, 2**32 - 1
     if not isinstance(addr_int, int):
         raise TypeError("Input must be of type 'int'")
-    if addr_int < ipv4_min or addr_int > ipv4_max:
+    if addr_int < IPV4_MIN_VAL or addr_int > IPV4_MAX_VAL:
         raise ValueError("Input not within IPv4 address range")
-
-    byte = 2**8 - 1
-    byte_len = 8
 
     addr_bytes = []
     for _ in range(4):
-        addr_bytes.append(addr_int & byte)
-        addr_int >>= byte_len
+        addr_bytes.append(addr_int & BYTE_MAX_SIZE)
+        addr_int >>= BYTE_SIZE
 
     return ".".join([str(byte) for byte in addr_bytes[::-1]])
 
@@ -66,8 +64,7 @@ def lower(address, mask):
 
 def upper(address, mask):
     """Return upper bound of the given subnet"""
-    ipv4_max = 32
-    tail = ipv4_max - mask
+    tail = IPV4_MAX_LEN - mask
 
     address = ipaddr(address)
     mask = netmask(mask)
