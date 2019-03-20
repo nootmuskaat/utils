29ec08a532f3f4ab86c1e7f23cc413fbeddb8457
richmond <andrew.richmond@nokia.com>
2019-03-20 08:34:49 +0100

    Refactored screen-brightness

--- screen-brightness.sh
+++ screen-brightness.sh
@@ -7,43 +7,55 @@ function show_usage() {
     >&2 echo "Usage: $0 [+|-]"
 }
 
-if [ -z "$1" -o "$1" == "-h" ]; then
+min() {
+    local -ri first=$1
+    local -ri second=$2
+    echo $(( first < second ? first : second ))
+}
+
+max() {
+    local -ri first=$1
+    local -ri second=$2
+    echo $(( first > second ? first : second ))
+}
+
+main() {
+    local -r direction="$1"
+
+    local -r backlight_dir=/sys/class/backlight/intel_backlight
+    local -r current_value_file="${backlight_dir}/brightness"
+
+    if [[ ! -d "${backlight_dir}" ]] ; then
+        logger "ERROR: Cannot adjust brightness - no directory ${backlight_dir}"
+        return 404
+    fi
+
+    local -ri min_brightness=0
+    local -ri max_brightness=$(cat "${backlight_dir}/max_brightness")
+    local -ri current_brightness=$(cat "${current_value_file}")
+
+    local -i increment=$(( max_brightness / 20 ))
+    increment=$(max ${increment} 1)
+
+    local -i new_brightness=$(( current_brightness $direction increment ))
+    new_brightness=$(min ${max_brightness} ${new_brightness})
+    new_brightness=$(max ${min_brightness} ${new_brightness})
+
+    echo ${new_brightness} > "${current_value_file}"
+    if ! grep -q "^${new_brightness}$" "${current_value_file}" ; then
+        logger "ERROR: Failed to set screen brightness"
+        return 1
+    fi
+
+}
+
+if [[ -z "$1" || "$1" == "-h" ]] ; then
     show_usage
-    exit 1
-elif [ "$1" != "+" -a "$1" != "-" ]; then
+    exit 0
+elif [[ "$1" != "+" && "$1" != "-" ]] ; then
     >&2 echo "Invalid argument: '$1'"
     show_usage
     exit 1
-else
-    DIRECTION=$1
 fi
 
-BACKLIGHT_DIR=/sys/class/backlight/intel_backlight
-if [ ! -d $BACKLIGHT_DIR ]; then 
-    logger "ERROR: $BACKLIGHT_DIR not found!"
-    exit 404
-fi
-
-## Set increment to ~2% total brightness, minimum value of 1
-MAX_BRIGHTNESS=$(cat $BACKLIGHT_DIR/max_brightness)
-INCR=$(expr 2 \* $MAX_BRIGHTNESS \/ 100)
-if [ $INCR -eq 0 ]; then
-    INCR=1
-fi
-
-
-CURRENT=$(cat $BACKLIGHT_DIR/brightness)
-NEW=$(expr $CURRENT $DIRECTION $INCR)
-
-function set_brightness() {
-    echo $1 > $BACKLIGHT_DIR/brightness
-    # verify the unlikely scenario that the change didn't take effect
-    NOW=$(cat $BACKLIGHT_DIR/brightness)
-    if [ $NOW -ne $1 ]; then 
-        return 1
-    fi
-}
-
-if [ $NEW -ge 0 -a $NEW -le $MAX_BRIGHTNESS ]; then
-    set_brightness $NEW || logger "Failed to adjust screen brightness"
-fi
+main "$@"
